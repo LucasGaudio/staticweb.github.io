448fa47eef3ca48e493723e534d54b1f255781dd
LucasGaudio <39922894+LucasGaudio@users.noreply.github.com>
2019-08-21 00:21:40 -0300

    Add files via upload

---
+++ Garota_Desenho_Feira/precache-manifest.aa2399842f2f24d4f89ec6d90d3ec240.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc93e96bb1a6d0caeb40e06a828a765b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b83e419830017c48c13",
+    "url": "/static/css/main.39de6944.chunk.css"
+  },
+  {
+    "revision": "9227f3c69434129a2b82",
+    "url": "/static/js/2.1e84fbf0.chunk.js"
+  },
+  {
+    "revision": "3b83e419830017c48c13",
+    "url": "/static/js/main.d6160c97.chunk.js"
+  },
+  {
+    "revision": "6f493329361ad8757612",
+    "url": "/static/js/runtime~main.e37e5d66.js"
+  },
+  {
+    "revision": "2fe756f3c49b82fb6c16508636450855",
+    "url": "/static/media/slide_one.2fe756f3.jpg"
+  },
+  {
+    "revision": "765f02c2c343e98d3ab40611a160b950",
+    "url": "/static/media/slide_three.765f02c2.jpg"
+  },
+  {
+    "revision": "5d771c1730ca2d522473129c79f60df2",
+    "url": "/static/media/slide_two.5d771c17.jpg"
+  }
+]);
